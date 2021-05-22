f7b1936320308b60237887b2b480f858991f03be
Drawiin <37352066+Drawiin@users.noreply.github.com>
2021-05-22 15:08:38 -0400

    Create lesson_1.js

---
+++ lessons/lesson_1.js
@@ -0,0 +1,181 @@
+{
+  "id": 1,
+  "title": "Jetpack Compose",
+  "image": "img_1",
+  "levels": [
+    [
+      {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      }
+    ],
+    [
+      {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      },
+       {
+        "question": "How to change the screen in jetpack compos ?",
+        "answers": [
+          {
+            "right": true,
+            "body": "Change the state"
+          },
+          {
+            "right": false,
+            "body": "Calling invalidate()"
+          },
+          {
+            "right": false,
+            "body": "Calling update() inside a composable"
+          }
+        ]
+      }
+    ]
+  ]
+}
